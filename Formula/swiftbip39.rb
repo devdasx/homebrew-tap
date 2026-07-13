1230965a36b604be3dce407547e4f27f0768f404
devdasx <devdas98x@gmail.com>
2026-07-13 16:28:28 +0800

    Update swiftbip39 to 1.1.2

--- Formula/swiftbip39.rb
+++ Formula/swiftbip39.rb
@@ -1,8 +1,8 @@
 class Swiftbip39 < Formula
   desc "BIP-39 mnemonic generator, validator, and seed derivation CLI"
   homepage "https://github.com/devdasx/swift-bip39-mnemonic"
-  url "https://github.com/devdasx/swift-bip39-mnemonic/archive/refs/tags/1.1.1.tar.gz"
-  sha256 "58bec78944166896ebb12003e64ec1a8b20a48b12042f31871c7b1fc13dace54"
+  url "https://github.com/devdasx/swift-bip39-mnemonic/archive/refs/tags/1.1.2.tar.gz"
+  sha256 "7a62f212418996e68e4e1eebe72ff508b5d5a935ec1b99318a003baf8e853b65"
   license "MIT"
 
   depends_on xcode: ["16.0", :build]
@@ -11,6 +11,7 @@ class Swiftbip39 < Formula
   def install
     system "swift", "build", "-c", "release", "--disable-sandbox"
     bin.install ".build/release/swiftbip39"
+    bin.install ".build/release/SwiftBIP39_SwiftBIP39.bundle"
   end
 
   test do
